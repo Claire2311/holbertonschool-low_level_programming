017b2c3c4cb10de1de1b13baaae0ae1d47265512
Claire2311 <claire.girard@ieseg.fr>
2024-04-01 19:43:11 +0200

    Task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+/**
+ * print_rev - function that returns the length of a string
+ * @s: string we want to reverse
+ *
+ * Return: integer with the number of char
+ */
+
+int _strlen(char *s)
+{
+int count = 0;
+
+while (*s != '\0')
+{
+count++;
+s++;
+}
+
+return (count);
+}
+
+void print_rev(char *s)
+
+x = _strlen( *s);
+
+while (x >= 0)
+{
+_putchar(s[x]);
+x--;
+}
+
+_putchar('\n');
+
+}
---
+++ pointers_arrays_strings/4-print_rev.h
@@ -0,0 +1,13 @@
+//
+//  4-print_rev.h
+//  
+//
+//  Created by Claire Girard on 01/04/2024.
+//
+
+#ifndef __print_rev_h
+#define __print_rev_h
+
+#include <stdio.h>
+
+#endif /* __print_rev_h */
