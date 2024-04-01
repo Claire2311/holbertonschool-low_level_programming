b43d9c29a4ebd4aa21ab06c9f62416c7f23a884f
Claire2311 <claire.girard@ieseg.fr>
2024-04-01 17:44:26 +0200

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string we want the length
+ *
+ * Return: integer with the number of char
+ */
+int _strlen(char *s);
+{
+return strlen(s);
+}
---
+++ pointers_arrays_strings/2-strlen.h
@@ -0,0 +1,13 @@
+//
+//  2-strlen.h
+//  
+//
+//  Created by Claire Girard on 01/04/2024.
+//
+
+#ifndef __strlen_h
+#define __strlen_h
+
+#include <stdio.h>
+
+#endif /* __strlen_h */
