ea60205b5e8b86fa06d2ae1ba94f1e2bf8290ae9
Claire2311 <claire.girard@ieseg.fr>
2024-04-01 16:27:08 +0200

    Task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+//
+//  0-reset_to_98.c
+//  
+//
+//  Created by Claire Girard on 01/04/2024.
+//
+
+#include "0-reset_to_98.h"
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ pointers_arrays_strings/0-reset_to_98.h
@@ -0,0 +1,13 @@
+//
+//  0-reset_to_98.h
+//  
+//
+//  Created by Claire Girard on 01/04/2024.
+//
+
+#ifndef __reset_to_98_h
+#define __reset_to_98_h
+
+#include <stdio.h>
+
+#endif /* __reset_to_98_h */
