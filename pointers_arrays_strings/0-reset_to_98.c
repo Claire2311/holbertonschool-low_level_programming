ad1ebd85511cc9a379dbec54a3d5fe47127d04f5
Claire2311 <claire.girard@ieseg.fr>
2024-04-01 16:53:19 +0200

    Task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,7 @@
-//
-//  0-reset_to_98.c
-//  
-//
-//  Created by Claire Girard on 01/04/2024.
-//
-
-#include "0-reset_to_98.h"
+/**
+ * function that takes a pointer to an int as parameter and updates the value it points to to 98
+ *
+ */
 void reset_to_98(int *n)
 {
     *n = 98;
